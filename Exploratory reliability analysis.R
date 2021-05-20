c035580e5712836893a82292bc725c6973f0645e
andreamalekash <74975144+andreamalekash@users.noreply.github.com>
2021-05-20 15:11:59 -0500

    Update Exploratory reliability analysis.R

--- Exploratory reliability analysis.R
+++ Exploratory reliability analysis.R
@@ -24,7 +24,7 @@ boxplot(Score ~ TA, data = scores)
 model.TA <- aov(Score ~ TA, data= scores)
 summary(model.TA)
 
-model.TA <- aov(AveScore ~ TA, data= Fall)
+model.TA <- aov(AveScore ~ TA, data= Spring)
 summary(model.TA)
 
 #Checking ANOVA assumptions and possible problem spots
@@ -81,6 +81,9 @@ VarCorr(mod)
 
 mod1 <- lme(Score1~1, data=Fall, random=~1|TA/Lab)
 VarCorr(mod1)
+
+mode3level <- lme(Score~1, data=Spring_l, random=~1|TA/Lab/Time) #Have to use these to do manual calculations for level specific ICC
+VarCorr(mode3level)
 
 #To meaningfully the TA's meaningfully, I want to choose a  TA to be the reference group, one which has a group mean close to the grand mean.
 SpringScore1.summary <- describeBy(x=Spring$Score1, group=Spring$TA, mat=TRUE) #I choose TA B for Spring
@@ -155,7 +158,10 @@ fligner.test(Score1~Semester, data=subset(All, TA == "J", select= c(Score1, Seme
 fligner.test(Score1~Semester, data=All)
 bartlett.test(Score1~Semester, data=All)
 
-######################Plots##################################333
+######################Plots##################################
+All$Semester <- factor(All$Semester,levels = c("Spring", "Fall")) #Change order 
+All_l$Semester <- factor(All_l$Semester,levels = c("Spring", "Fall")) #Change order 
+
 #Score 1 by Semester 
 ggplot(All, aes(x = Score2, fill = Semester)) +
   geom_density(alpha = .3) +
@@ -174,3 +180,132 @@ ggplot(subset(All_l, Semester == "Fall", select= c(Score, Time)), aes(x = Score,
   geom_density(alpha = .3) +
   labs(fill="Assignment")
 
+#Sort Spring to Fall to get re-ordered key (Spring then Fall)
+All_l %>%
+  mutate(Semester=fct_relevel(Semester,"Fall","Spring)"))
+
+#Assignment 1 Score by Semester
+ggplot(subset(All_l, Time ==1 , select= c(Score, Semester)), aes(x = Score, fill = Semester)) +
+  geom_density(alpha = .3) +
+  labs(fill="Semester", x="Assignment 1", y = "")
+
+#Assignment 2 Score by Semester
+ggplot(subset(All_l, Time == 2, select= c(Score, Semester)), aes(x = Score, fill = Semester)) +
+  geom_density(alpha = .3) +
+  labs(fill="Semester", x="Assignment 2", y="")
+
+#BOXPLOT Score 1 by semester and by TA
+All$AveScore <- All$Score1 + All$Score2
+
+ggplot(All,aes(x = TA, y=AveScore, fill = Semester)) +
+  geom_boxplot() +
+  labs(fill="Semester", y="Average of Assignments 1 and 2")
+##########Mean Adjustments###################################
+describeBy(Spring$Score1, Spring$TA)
+#Make a table with spring means for each TA
+by_TA <- Spring %>% group_by(TA) #grouping by TA first
+SpringMeans <-by_TA %>% dplyr::summarize(
+  mean1=mean(Score1), mean2 = mean(Score2)
+)
+#Make a table with fall means for each TA
+Fallby_TA <- Fall %>% group_by(TA) #grouping by TA first
+FallMeans <-Fallby_TA %>% dplyr::summarize(
+  mean1=mean(Score1), mean2 = mean(Score2)
+)
+
+#Create new column in "Spring" that include the mean-adjusted score
+Spring$Score1Mean=Spring$Score1 #new column for mean
+Spring$Score1Adj=Spring$Score1 #new column for adjusted score
+Spring$Score2Mean=Spring$Score2 #new column for mean
+Spring$Score2Adj=Spring$Score2 #new column for adjusted score
+
+#Take the mean of each TA and put it into the new column
+Spring$Score1Mean[which(Spring$TA=="A")]=SpringMeans$mean1[which(SpringMeans$TA=="A")]
+Spring$Score1Mean[which(Spring$TA=="B")]=SpringMeans$mean1[which(SpringMeans$TA=="B")]
+Spring$Score1Mean[which(Spring$TA=="C")]=SpringMeans$mean1[which(SpringMeans$TA=="C")]
+Spring$Score1Mean[which(Spring$TA=="D")]=SpringMeans$mean1[which(SpringMeans$TA=="D")]
+Spring$Score1Mean[which(Spring$TA=="E")]=SpringMeans$mean1[which(SpringMeans$TA=="E")]
+Spring$Score1Mean[which(Spring$TA=="F")]=SpringMeans$mean1[which(SpringMeans$TA=="F")]
+Spring$Score1Mean[which(Spring$TA=="H")]=SpringMeans$mean1[which(SpringMeans$TA=="H")]
+Spring$Score1Mean[which(Spring$TA=="I")]=SpringMeans$mean1[which(SpringMeans$TA=="I")]
+Spring$Score1Mean[which(Spring$TA=="J")]=SpringMeans$mean1[which(SpringMeans$TA=="J")]
+#score2
+Spring$Score2Mean[which(Spring$TA=="A")]=SpringMeans$mean2[which(SpringMeans$TA=="A")]
+Spring$Score2Mean[which(Spring$TA=="B")]=SpringMeans$mean2[which(SpringMeans$TA=="B")]
+Spring$Score2Mean[which(Spring$TA=="C")]=SpringMeans$mean2[which(SpringMeans$TA=="C")]
+Spring$Score2Mean[which(Spring$TA=="D")]=SpringMeans$mean2[which(SpringMeans$TA=="D")]
+Spring$Score2Mean[which(Spring$TA=="E")]=SpringMeans$mean2[which(SpringMeans$TA=="E")]
+Spring$Score2Mean[which(Spring$TA=="F")]=SpringMeans$mean2[which(SpringMeans$TA=="F")]
+Spring$Score2Mean[which(Spring$TA=="H")]=SpringMeans$mean2[which(SpringMeans$TA=="H")]
+Spring$Score2Mean[which(Spring$TA=="I")]=SpringMeans$mean2[which(SpringMeans$TA=="I")]
+Spring$Score2Mean[which(Spring$TA=="J")]=SpringMeans$mean2[which(SpringMeans$TA=="J")]
+#Subtract each person's Score1 from that new column and replace the value in the column with the new adjusted score
+Spring$Score1Adj <- (Spring$Score1)-(Spring$Score1Mean)
+Spring$Score2Adj <- (Spring$Score2)-(Spring$Score2Mean)
+#Now add Score1Adj to overall score 1 mean to get the new score
+Spring$Score1New <- (Spring$Score1Adj)+(mean(Spring$Score1))
+Spring$Score2New <- (Spring$Score2Adj)+(mean(Spring$Score2))
+
+#Create new columns in "Fall" data
+Fall$Score1Mean=Fall$Score1 #new column for mean
+Fall$Score1Adj=Fall$Score1 #new column for adjusted score
+Fall$Score2Mean=Fall$Score2 #new column for mean
+Fall$Score2Adj=Fall$Score2 #new column for adjusted score
+
+#Take the mean of each TA and put it into the new column
+Fall$Score1Mean[which(Fall$TA=="A")]=FallMeans$mean1[which(FallMeans$TA=="A")]
+Fall$Score1Mean[which(Fall$TA=="B")]=FallMeans$mean1[which(FallMeans$TA=="B")]
+Fall$Score1Mean[which(Fall$TA=="C")]=FallMeans$mean1[which(FallMeans$TA=="C")]
+Fall$Score1Mean[which(Fall$TA=="H")]=FallMeans$mean1[which(FallMeans$TA=="H")]
+Fall$Score1Mean[which(Fall$TA=="J")]=FallMeans$mean1[which(FallMeans$TA=="J")]
+Fall$Score1Mean[which(Fall$TA=="K")]=FallMeans$mean1[which(FallMeans$TA=="K")]
+Fall$Score1Mean[which(Fall$TA=="L")]=FallMeans$mean1[which(FallMeans$TA=="L")]
+Fall$Score1Mean[which(Fall$TA=="M")]=FallMeans$mean1[which(FallMeans$TA=="M")]
+Fall$Score1Mean[which(Fall$TA=="O")]=FallMeans$mean1[which(FallMeans$TA=="O")]
+Fall$Score1Mean[which(Fall$TA=="P")]=FallMeans$mean1[which(FallMeans$TA=="P")]
+Fall$Score1Mean[which(Fall$TA=="Q")]=FallMeans$mean1[which(FallMeans$TA=="Q")]
+#score2
+Fall$Score2Mean[which(Fall$TA=="A")]=FallMeans$mean2[which(FallMeans$TA=="A")]
+Fall$Score2Mean[which(Fall$TA=="B")]=FallMeans$mean2[which(FallMeans$TA=="B")]
+Fall$Score2Mean[which(Fall$TA=="C")]=FallMeans$mean2[which(FallMeans$TA=="C")]
+Fall$Score2Mean[which(Fall$TA=="H")]=FallMeans$mean2[which(FallMeans$TA=="H")]
+Fall$Score2Mean[which(Fall$TA=="J")]=FallMeans$mean2[which(FallMeans$TA=="J")]
+Fall$Score2Mean[which(Fall$TA=="K")]=FallMeans$mean2[which(FallMeans$TA=="K")]
+Fall$Score2Mean[which(Fall$TA=="L")]=FallMeans$mean2[which(FallMeans$TA=="L")]
+Fall$Score2Mean[which(Fall$TA=="M")]=FallMeans$mean2[which(FallMeans$TA=="M")]
+Fall$Score2Mean[which(Fall$TA=="O")]=FallMeans$mean2[which(FallMeans$TA=="O")]
+Fall$Score2Mean[which(Fall$TA=="P")]=FallMeans$mean2[which(FallMeans$TA=="P")]
+Fall$Score2Mean[which(Fall$TA=="Q")]=FallMeans$mean2[which(FallMeans$TA=="Q")]
+#Subtract each person's Score1 from that new column and replace the value in the column with the new adjusted score
+Fall$Score1Adj <- (Fall$Score1)-(Fall$Score1Mean)
+Fall$Score2Adj <- (Fall$Score2)-(Fall$Score2Mean)
+#Now add Score1Adj to overall score 1 mean to get the new score
+Fall$Score1New <- (Fall$Score1Adj)+(mean(Fall$Score1))
+Fall$Score2New <- (Fall$Score2Adj)+(mean(Fall$Score2))
+
+#Graphing new adjusted score distributions#################################
+Fall$TA <- factor(Fall$TA, levels=c("A","B","C","H","J","K","L","M","O","P","Q"))
+Spring$TA <- factor(Spring$TA, levels=c("A","B","C","D","E","F","H","I","J"))
+#Putting New scores into All_l
+All_l<- All_l[order(All_l$Student),]
+Fall<- Fall[order(Fall$Student),]
+Spring<- Spring[order(Spring$Student),] #ordering
+
+All_l$Score1New <- All_l$Score
+All_l$Score1New[which(All_l$Time==1)]=Spring$Score1New
+Fall$Score2Mean[which(Fall$TA=="A")]=FallMeans$mean2[which(FallMeans$TA=="A")]
+
+#Assignment 1 Score by Semester
+Fall$TA <- factor(Fall$TA, levels=c("A","B","C","H","J","K","L","M","O","P","Q"))
+ggplot(Fall,aes(x=TA, y=Score1New))+
+        geom_boxplot() +
+  labs(x="TA", y= "Score 1")
+#Assignment 1 Score by Semester
+Fall$TA <- factor(Fall$TA, levels=c("A","B","C","H","J","K","L","M","O","P","Q"))
+ggplot(Fall,aes(x=TA, y=Score2New))+
+  geom_boxplot() +
+  labs(x="TA", y= "Mean-Adjusted Score 2 (Fall)")
+#Assignment 2 Score by Semester
+ggplot(subset(All_l, Time == 2, select= c(Score, Semester)), aes(x = Score, fill = Semester)) +
+  geom_density(alpha = .3) +
+  labs(fill="Semester")
